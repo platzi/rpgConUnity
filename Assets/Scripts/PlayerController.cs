c80cb482d965678dac125b0ebc9ef39a9d719b19
Juan Gabriel <juangabriel@frogames.es>
2019-01-26 12:57:09 -0500

    Clase 12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,11 +16,12 @@ public class PlayerController : MonoBehaviour
     private const string walkingState = "Walking";
 
     private Animator animator;
-
+    private Rigidbody2D playerRigidbody;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -31,19 +32,35 @@ public class PlayerController : MonoBehaviour
 
         if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
-            this.transform.Translate(
+            /*this.transform.Translate(
                 new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,0,0));
+           */
+            playerRigidbody.velocity = new Vector2(
+                Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,
+                playerRigidbody.velocity.y);
             walking = true;
             lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
-            this.transform.Translate(
+            /*this.transform.Translate(
                 new Vector3(0, Input.GetAxisRaw(vertical)*speed*Time.deltaTime,0));
+            
+            */
+            playerRigidbody.velocity = new Vector2(
+                playerRigidbody.velocity.x,
+                Input.GetAxisRaw(vertical) * speed * Time.deltaTime
+                );
             walking = true;
             lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
+
+        if (!walking)
+        {
+            playerRigidbody.velocity = Vector2.zero;
+        }
+
 
         animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
         animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
