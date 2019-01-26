c4f0d4817cb7bf1a18d218bfb2e462602ef59d21
Juan Gabriel <juangabriel@frogames.es>
2019-01-26 12:11:48 -0500

    Clase 10

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject followTarget;
+    [SerializeField]
+    private Vector3 targetPosition;
+    [SerializeField]
+    private float cameraSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(followTarget.transform.position.x,
+                                     followTarget.transform.position.y,
+                                    this.transform.position.z);
+
+        this.transform.position = Vector3.Lerp(this.transform.position,
+                                              targetPosition,
+                                              cameraSpeed * Time.deltaTime);
+    }
+}
