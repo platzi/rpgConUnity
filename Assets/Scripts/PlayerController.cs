41d7ff6f1f6e93a5ada3139d208bf73a03ad135a
Juan Gabriel <juangabriel@frogames.es>
2019-01-26 11:28:02 -0500

    Clase 7

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // s = v*t;
+
+        if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        {
+            this.transform.Translate(
+                new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,0,0));
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+        {
+            this.transform.Translate(
+                new Vector3(0, Input.GetAxisRaw(vertical)*speed*Time.deltaTime,0));
+        }
+
+        animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
+        animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+
+    }
+}
