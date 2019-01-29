02bae7ac31fbd65cb520d354c4a3ce2ead604b05
Juan Gabriel <juangabriel@frogames.es>
2019-01-29 11:10:29 -0500

    Clase 34

--- Assets/Scripts/DamageNumber.cs
+++ Assets/Scripts/DamageNumber.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageNumber : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damageSpeed;
+    public float damagePoints;
+
+    public Text damageText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        damageText.text = damagePoints.ToString();
+        this.transform.position = new Vector3(
+                this.transform.position.x,
+                this.transform.position.y + damageSpeed * Time.deltaTime,
+                this.transform.position.z
+            );
     }
 }
--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -8,6 +8,7 @@ public class WeaponDamage : MonoBehaviour
 
     public GameObject hurtAnimation;
     public GameObject hitPoint;
+    public GameObject damageNumber;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +18,12 @@ public class WeaponDamage : MonoBehaviour
                 .DamageCharacter(damage);
             Instantiate(hurtAnimation, hitPoint.transform.position,
                         hitPoint.transform.rotation);
+
+            var clone = (GameObject)Instantiate(damageNumber,
+                    hitPoint.transform.position,
+                    Quaternion.Euler(Vector3.zero)
+                );
+            clone.GetComponent<DamageNumber>().damagePoints = damage;
         }
     }
 }
