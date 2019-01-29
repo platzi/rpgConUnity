cb3c09d6239c81b6b1075923a7924fa00b2dc1b9
Juan Gabriel <juangabriel@frogames.es>
2019-01-29 09:24:40 -0500

    Clase 29

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 4.0f;
-    private float currentSpeed;
 
     private bool walking = false;
     public Vector2 lastMovement = Vector2.zero;
@@ -73,45 +72,17 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-
-
-
-
-            if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+            if(Mathf.Abs(Input.GetAxisRaw(horizontal))>0.5f ||
+                Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5)
             {
-                /*this.transform.Translate(
-                    new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime,0,0));
-               */
-                playerRigidbody.velocity = new Vector2(
-                    Input.GetAxisRaw(horizontal) * currentSpeed * Time.deltaTime,
-                    playerRigidbody.velocity.y);
                 walking = true;
-                lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
-            }
 
-            if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
-            {
-                /*this.transform.Translate(
-                    new Vector3(0, Input.GetAxisRaw(vertical)*speed*Time.deltaTime,0));
+                lastMovement = new Vector2(
+                    Input.GetAxisRaw(horizontal),
+                    Input.GetAxisRaw(vertical));
 
-                */
-                playerRigidbody.velocity = new Vector2(
-                    playerRigidbody.velocity.x,
-                    Input.GetAxisRaw(vertical) * currentSpeed * Time.deltaTime
-                    );
-                walking = true;
-                lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
-            }
-
-
-            if(Mathf.Abs(Input.GetAxisRaw(horizontal))>0.5f&&
-              Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
-            {
-                currentSpeed = speed / Mathf.Sqrt(2);
-            }
-            else
-            {
-                currentSpeed = speed;
+                playerRigidbody.velocity = lastMovement.normalized *
+                    speed * Time.deltaTime;
             }
         }
 
