80d798c554981901416d39b32d998c2f7ed3317e
Juan Gabriel <juangabriel@frogames.es>
2019-01-30 10:21:54 -0500

    Clase 42

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public bool dialogActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            dialogActive = false;
+            dialogBox.SetActive(false);
+        }
+    }
+
+    public void ShowDialog(string text)
+    {
+        dialogActive = true;
+        dialogBox.SetActive(true);
+        dialogText.text = text;
+    }
+}
