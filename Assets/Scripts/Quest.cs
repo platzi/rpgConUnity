e357dba74162d1383cea811bff80aede4a6e7aff
Juan Gabriel <juangabriel@frogames.es>
2019-01-30 11:41:07 -0500

    Clase 47

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    public int questID;
+    private QuestManager manager;
+
+    public string startText, completeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<QuestManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartQuest()
+    {
+        manager.ShowQuestText(startText);
+    }
+
+    public void CompleteQuest()
+    {
+        manager.ShowQuestText(completeText);
+        manager.questCompleted[questID] = true;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public Quest[] quests;
+    public bool[] questCompleted;
+
+    private DialogManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questCompleted = new bool[quests.Length];
+        manager = FindObjectOfType<DialogManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowQuestText(string questText)
+    {
+        string[] dialogLines = new string[]
+        {
+            questText
+        };
+        manager.ShowDialog(dialogLines);
+    }
+}
