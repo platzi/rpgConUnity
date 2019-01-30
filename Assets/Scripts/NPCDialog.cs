3633a86eabec2101b4f4cdb5459ff678fb49756e
Juan Gabriel <juangabriel@frogames.es>
2019-01-30 10:41:04 -0500

    Clase 43

---
+++ Assets/Scripts/NPCDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialog : MonoBehaviour
+{
+    public string dialog;
+    private DialogManager manager;
+    private bool playerInTheZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<DialogManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            playerInTheZone = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInTheZone && Input.GetKeyDown(KeyCode.Return))
+        {
+            manager.ShowDialog(dialog);
+        }
+    }
+}
