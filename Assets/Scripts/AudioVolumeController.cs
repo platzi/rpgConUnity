0608e547086b18209fc182dd6004750201e1d7df
Juan Gabriel <juangabriel@frogames.es>
2019-01-31 11:09:32 -0500

    Clase 56

---
+++ Assets/Scripts/AudioVolumeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float currentAudioLevel;
+    public float defaultAudioLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); 
+    }
+
+    public void SetAudioLevel(float newVolume)
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        currentAudioLevel = defaultAudioLevel * newVolume;
+        audioSource.volume = currentAudioLevel;
+    }
+}
---
+++ Assets/Scripts/AudioVolumeManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeManager : MonoBehaviour
+{
+    private AudioVolumeController[] audios;
+    public float maxVolumeLevel;
+    public float currentVolumeLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audios = FindObjectsOfType<AudioVolumeController>();
+        ChangeGlobalAudioVolume();
+    }
+
+    private void Update()
+    {
+        ChangeGlobalAudioVolume();
+    }
+
+    public void ChangeGlobalAudioVolume()
+    {
+        if(currentVolumeLevel >= maxVolumeLevel)
+        {
+            currentVolumeLevel = maxVolumeLevel;
+        }
+
+        foreach(AudioVolumeController avc in audios)
+        {
+            avc.SetAudioLevel(currentVolumeLevel);
+        }
+
+    }
+}
