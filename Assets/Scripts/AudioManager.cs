ac514b35304199afdd06b8eaa052f01bdbc05db2
Juan Gabriel <juangabriel@frogames.es>
2019-01-31 10:44:01 -0500

    Clase 55

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource[] audioTracks;
+    public int currentTrack;
+    public bool audioCanBePlayed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioCanBePlayed)
+        {
+            if (!audioTracks[currentTrack].isPlaying)
+            {
+                audioTracks[currentTrack].Play();
+            }
+        }
+    }
+
+    public void PlayNewTrack(int newTrack)
+    {
+        audioTracks[currentTrack].Stop();
+        currentTrack = newTrack;
+        audioTracks[currentTrack].Play();
+    }
+}
---
+++ Assets/Scripts/AudioNewTrack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioNewTrack : MonoBehaviour
+{
+
+    private AudioManager manager;
+    public int newTrackID;
+    public bool playOnStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<AudioManager>();
+        if (playOnStart)
+        {
+            manager.PlayNewTrack(newTrackID);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            manager.PlayNewTrack(newTrackID);
+            gameObject.SetActive(false);
+        }
+    }
+}
