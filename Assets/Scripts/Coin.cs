7a2149e5dee1372f6f142d6c49d3354d5894d87a
Juan Gabriel <juangabriel@frogames.es>
2019-01-31 11:28:04 -0500

    Clase 57

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+    private CoinsManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<CoinsManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            manager.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsManager : MonoBehaviour
+{
+    public Text moneyText;
+    public int currentGold;
+
+    private const string goldkey = "CurrentGold";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(goldkey))
+        {
+            currentGold = PlayerPrefs.GetInt(goldkey);
+        }
+        else
+        {
+            currentGold = 0;
+            PlayerPrefs.SetInt(goldkey, 0);
+        }
+        moneyText.text = currentGold.ToString();
+    }
+
+    public void AddMoney(int moneyCollected)
+    {
+        currentGold += moneyCollected;
+        PlayerPrefs.SetInt(goldkey, currentGold);
+        moneyText.text = currentGold.ToString();
+    }
+}
